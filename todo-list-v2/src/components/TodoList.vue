7eec1de2838c987c213dbb59ba7c44d9c73f134a
uxedj <edigabrielcontato@hotmail.com>
2022-12-16 15:18:47 -0300

    iniciando todo-list

--- todo-list-v2/src/components/TodoList.vue
+++ todo-list-v2/src/components/TodoList.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="">
-        <div class="container ">
-            <h3 class=" text-red-500">Todo List</h3>
-            <p>teste</p>
+        <div class="container mx-auto row">
+            <h3 class="text-xl font-semibold text-center text-teal-500">Todo List</h3>
             <input
-                class="borde-neutral-500" 
+                @keyup.enter="addNewItemToList"
+                class="mx-auto border-2 rounded-lg md:col-4 bg-slate-200 border-slate-500" 
                 type="text"
             >
             <ul>
+                <li
+                    class="text-center"
+                    v-for="(item, index) in list" :key="index"
+                >
+                    <input type="checkbox" v-model="item.checked">
+                    {{ item.label}}
+                </li>    
             </ul>
-            <div>agora</div>
-
+            
+ 
         </div>
     </div>
 </template>
@@ -18,8 +25,27 @@
 <script>
 export default {
     name: 'TodoList',
-    props: {
+    data() {
+        return {
+            list: [
+                
+            ]
+        }
+    },
+    created() {
+        const itensInLocalStorage = JSON.parse(localStorage.getItem('list'));
+        this.list = itensInLocalStorage ? itensInLocalStorage : [];
+    },
+    methods: {
+        addNewItemToList(event) {
+            const newItem = event.target.value;
+            this.list.unshift({
+                label: newItem, checked: false
+            })
 
+            localStorage.setItem('list', JSON.stringify(this.list));
+            event.target.value = '';
+        }
     }
 }
 </script>
