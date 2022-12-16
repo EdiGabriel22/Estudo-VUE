0b85c1005fb1e288903899cafa79aab0422e8bac
Edi Gabriel <edigabrielcontato@hotmail.com>
2022-12-15 22:17:55 -0300

    todo-list-vue2

---
+++ todo-list-v2/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <div class="container ">
+            <h3 class=" text-red-500">Todo List</h3>
+            <p>teste</p>
+            <input
+                class="borde-neutral-500" 
+                type="text"
+            >
+            <ul>
+            </ul>
+            <div>agora</div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    props: {
+
+    }
+}
+</script>
