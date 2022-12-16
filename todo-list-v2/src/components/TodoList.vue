398ce96edf8cfe57010b6af47c65338914e43e16
uxedj <edigabrielcontato@hotmail.com>
2022-12-16 18:07:28 -0300

    iniciando-style

--- todo-list-v2/src/components/TodoList.vue
+++ todo-list-v2/src/components/TodoList.vue
@@ -2,22 +2,39 @@
     <div class="">
         <div class="container mx-auto row">
             <h3 class="text-xl font-semibold text-center text-teal-500">Todo List</h3>
+            <i class="fa-solid fa-flag"></i>
             <input
                 @keyup.enter="addNewItemToList"
-                class="mx-auto border-2 rounded-lg md:col-4 bg-slate-200 border-slate-500" 
+                class="mx-auto border-2 rounded-lg md:col-4 bg-slate-200 border-slate-500"
                 type="text"
             >
             <ul>
                 <li
-                    class="text-center"
-                    v-for="(item, index) in list" :key="index"
+                    class="flex justify-between text-center"
+                    v-for="(item, index) in list"
+                    :key="index"
                 >
-                    <input type="checkbox" v-model="item.checked">
-                    {{ item.label}}
-                </li>    
+                    <div>
+                        <input
+                            :id="index"
+                            @click="updateLocalStorage()"
+                            type="checkbox"
+                            v-model="item.checked"
+                        >
+                            <label
+                                :for="index"
+                                :class="{
+                                    'line-through' : item.checked
+                                }"
+                            >
+                                {{ item.label }}
+                            </label>
+                    </div>
+                        <span @click="deleteItem(index)">Excluir</span>
+                </li>
             </ul>
-            
- 
+
+
         </div>
     </div>
 </template>
@@ -28,7 +45,7 @@ export default {
     data() {
         return {
             list: [
-                
+
             ]
         }
     },
@@ -36,16 +53,23 @@ export default {
         const itensInLocalStorage = JSON.parse(localStorage.getItem('list'));
         this.list = itensInLocalStorage ? itensInLocalStorage : [];
     },
+
     methods: {
         addNewItemToList(event) {
             const newItem = event.target.value;
             this.list.unshift({
                 label: newItem, checked: false
             })
-
-            localStorage.setItem('list', JSON.stringify(this.list));
+            this.updateLocalStorage();
             event.target.value = '';
+        },
+        deleteItem(index) {
+            this.list.splice(index,1);
+            this.updateLocalStorage()
+        },
+        updateLocalStorage() {
+            localStorage.setItem('list', JSON.stringify(this.list));
         }
-    }
+    },
 }
 </script>
