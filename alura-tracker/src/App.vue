0e28e3b279d063ff3133d41d664945e65db1ce06
unknown <edigabrielcontato@hotmail.com>
2023-01-04 22:39:38 -0300

    iniciando vue alura

---
+++ alura-tracker/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<main class="columns is-gapless is-multiline">
+		<div class="columns is-one-quarter">
+			<BarraLateralVue />
+		</div>
+		<div class="columns is-three-quarter">
+			<FormularioVue/>
+		</div>
+	</main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import BarraLateralVue from './components/BarraLateral.vue';
+import FormularioVue from './components/Formulario.vue'
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		BarraLateralVue,
+		FormularioVue
+	}
+});
+</script>
+
+<style scoped>
+
+</style>
