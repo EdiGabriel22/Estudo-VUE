4dd6eb221d8842d4798f54a23cbf058ba43753e7
unknown <edigabrielcontato@hotmail.com>
2023-01-11 21:03:27 -0300

    iniciando reatividade

--- alura-tracker/src/views/Projetos/Formulario.vue
+++ alura-tracker/src/views/Projetos/Formulario.vue
@@ -22,6 +22,7 @@ import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 import useNotificador from "@/hooks/notificador";
 import { CADASTRAR_PROJETOS, ALTERAR_PROJETOS } from "@/store/tipo-acoes";
+// import { useRouter } from "vue-router";
 
 export default defineComponent({
 	name: "Formulario",
@@ -43,33 +44,11 @@ export default defineComponent({
 	// 		nomeDoProjeto: "",
 	// 	};
 	// },
-	methods: {
-		salvar() {
-			if (this.id) {
-				this.store.dispatch(ALTERAR_PROJETOS, {
-					id: this.id,
-					nome: this.nomeDoProjeto,
-				}).then(() => this.lidarComSucesso())
-			} else {
-				this.store.dispatch(CADASTRAR_PROJETOS, this.nomeDoProjeto)
-				.then(() => this.lidarComSucesso())
-			}
-		},
-		lidarComSucesso() {
-			this.nomeDoProjeto = "";
-				this.notificar(
-				TipoNotificacao.SUCESSO,
-				"Excelente!",
-				"O projeto foi cadastrado com sucesso!"
-				);
-				this.$router.push("/projetos");
-		}
-	},
 	setup(props) {
-		
+		// const router = useRouter
+
 		const store = useStore();
 		const { notificar } = useNotificador();
-
 
 		const nomeDoProjeto = ref("")
 
@@ -80,12 +59,31 @@ export default defineComponent({
 			nomeDoProjeto.value = projeto?.nome || "";
 		}
 
+		const lidarComSucesso = () => {
+			nomeDoProjeto.value = "";
+				notificar(
+				TipoNotificacao.SUCESSO,
+				"Excelente!",
+				"O projeto foi cadastrado com sucesso!"
+				);
+				// router.push("/projetos");
+		}
 
+		const salvar = () => {
+			if (props.id) {
+				store.dispatch(ALTERAR_PROJETOS, {
+					id: props.id,
+					nome: nomeDoProjeto.value,
+				}).then(() => lidarComSucesso())
+			} else {
+				store.dispatch(CADASTRAR_PROJETOS, nomeDoProjeto.value)
+				.then(() => lidarComSucesso())
+			}
+		}
 
 		return {
-			store,
-			notificar,
-			nomeDoProjeto
+			nomeDoProjeto,
+			salvar
 		};
 	},
 });
--- alura-tracker/src/views/Tarefas.vue
+++ alura-tracker/src/views/Tarefas.vue
@@ -6,13 +6,30 @@
 				>Você não está muito produtivo hoje:(</span
 			>
 		</Box>
+		<div class="field">
+			<p class="control has-icons-left">
+				<input
+					class="input"
+					type="text"
+					placeholder="Digite para filtrar"
+					v-model="filtro"
+				/>
+				<span class="icon is-small is-left">
+					<i class="fas fa-search"></i>
+				</span>
+			</p>
+		</div>
 		<Tarefa
 			v-for="(tarefa, index) in tarefas"
 			:tarefa="tarefa"
 			:key="index"
 			@aoTarefaClicada="selecionarTarefa"
 		/>
-		<div class="modal" :class="{'is-active': tarefaSelecionada}" v-if="tarefaSelecionada">
+		<div
+			class="modal"
+			:class="{ 'is-active': tarefaSelecionada }"
+			v-if="tarefaSelecionada"
+		>
 			<div class="modal-background"></div>
 			<div class="modal-card">
 				<header class="modal-card-head">
@@ -37,7 +54,9 @@
 					</div>
 				</section>
 				<footer class="modal-card-foot">
-					<button class="button is-success" @click="alterarTarefa">Salvar alterações</button>
+					<button class="button is-success" @click="alterarTarefa">
+						Salvar alterações
+					</button>
 					<button class="button" @click="fecharModal">
 						Cancelar
 					</button>
@@ -48,59 +67,71 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, computed} from "vue"
-import Formulario from "../components/Formulario.vue"
-import Tarefa from "../components/Tarefa.vue"
-import Box from "../components/Box.vue"
-import {useStore} from "@/store"
-import {
-	CADASTRAR_TAREFA,
-	OBTER_TAREFAS,
-	OBTER_PROJETOS,
-ALTERAR_TAREFA,
-} from "@/store/tipo-acoes"
-import ITarefa from "@/interfaces/ITarefa"
+	import { defineComponent, computed, ref } from "vue";
+	import Formulario from "../components/Formulario.vue";
+	import Tarefa from "../components/Tarefa.vue";
+	import Box from "../components/Box.vue";
+	import { useStore } from "@/store";
+	import {
+		CADASTRAR_TAREFA,
+		OBTER_TAREFAS,
+		OBTER_PROJETOS,
+		ALTERAR_TAREFA,
+	} from "@/store/tipo-acoes";
+	import ITarefa from "@/interfaces/ITarefa";
 
-export default defineComponent({
-	name: "App",
-	components: {
-		Formulario,
-		Tarefa,
-		Box,
-	},
-	data() {
-		return {
-			tarefaSelecionada: null as ITarefa | null,
-		}
-	},
-	methods: {
-		salvarTarefa(tarefa: ITarefa): void {
-			this.store.dispatch(CADASTRAR_TAREFA, tarefa)
+	export default defineComponent({
+		name: "App",
+		components: {
+			Formulario,
+			Tarefa,
+			Box,
 		},
-		selecionarTarefa(tarefa: ITarefa) {
-			this.tarefaSelecionada = tarefa
+		data() {
+			return {
+				tarefaSelecionada: null as ITarefa | null,
+			};
 		},
-		fecharModal() {
-			this.tarefaSelecionada = null
+		methods: {
+			salvarTarefa(tarefa: ITarefa): void {
+				this.store.dispatch(CADASTRAR_TAREFA, tarefa);
+			},
+			selecionarTarefa(tarefa: ITarefa) {
+				this.tarefaSelecionada = tarefa;
+			},
+			fecharModal() {
+				this.tarefaSelecionada = null;
+			},
+			alterarTarefa() {
+				this.store
+					.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+					.then(() => this.fecharModal);
+			},
 		},
-		alterarTarefa() {
-			this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
-				.then(() => this.fecharModal)
-		}
-	},
-	computed: {
-		semTarefas(): boolean {
-			return this.tarefas.length == 0
+		computed: {
+			semTarefas(): boolean {
+				return this.tarefas.length == 0;
+			},
 		},
-	},
-	setup() {
-		const store = useStore()
-		store.dispatch(OBTER_TAREFAS)
-		store.dispatch(OBTER_PROJETOS)
-		return {
-			tarefas: computed(() => store.state.tarefas),
-			store,
-		}
-	},
-})
+
+		setup() {
+			const store = useStore();
+			store.dispatch(OBTER_TAREFAS);
+			store.dispatch(OBTER_PROJETOS);
+
+			const filtro = ref("");
+
+			const tarefas = computed(() =>
+				store.state.tarefas.filter(
+					(t) => !filtro.value || t.descricao.includes(filtro.value)
+				)
+			);
+
+			return {
+				tarefas,
+				store,
+				filtro,
+			};
+		},
+	});
 </script>
